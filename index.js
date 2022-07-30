4e9094fc6b192397d825642cecb14152bbc6b5a1
konstantin <makarovestates@gmail.com>
2022-07-30 17:00:36 -0500

    commit tree c19dfe270579d5db1e9b5fb42f2d0dacb9372e70

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const main = document.getElementById("main"); 
+main.remove(); 
+
+
+const newHeader = document.createElement("h1"); 
+document.body.append(newHeader); 
+
+newHeader.nodeName = h1; 
+newHeader.className = "victory"; 
+newHeader.id = "victory"; 
+
+
